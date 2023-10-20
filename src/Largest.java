c03fa2e948891b098ae52ab334d53356ee83e311
ankit <@ankitkumar1302>
2023-10-20 18:27:36 +0530

    added College work

---
+++ src/Largest.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Largest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+        // Q. Find the largest of the three numbers.
+        // Write your code here.
+//        if(a>b && a>c)
+//        {
+//            System.out.println(a);
+//        }
+//        else if(b>a && b>c)
+//        {
+//            System.out.println(b);
+//        }
+//        else
+//        {
+//            System.out.println(c);
+//        }
+//        int max = a;
+//        if (b > a){
+//            max = b;
+//        }
+//        if (c > a){
+//            max = c;
+//        }
+//
+//        System.out.println(max);
+
+
+
+
+    }
+}
