c7ee62a6a85083c3a14f45e14845c8f5f88cbd3a
ankit <@ankitkumar1302>
2023-10-21 20:52:19 +0530

    calculator

--- src/fibonacchi.java
+++ src/fibonacchi.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
 
 public class fibonacchi {
+    /**
+     *
+     */
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int p = 0;
-        int i = 1;
+        int a = 0;
+        int b = 1;
         int count = 2;
 
         while (count <= n) {
-            int temp = i;
-            i = i + p;
-            p = temp;
+            int temp = b;
+            b = b + a;
+            a = temp;
             count++;
         }
-        System.out.println(i);
+        System.out.println(b);
     }
 }
