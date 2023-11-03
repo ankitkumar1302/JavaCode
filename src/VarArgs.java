8b7a4e6d27791be2b09c807b3edbdd14390a1e31
ankit <@ankitkumar1302>
2023-11-03 18:08:03 +0530

    Overloading

--- src/VarArgs.java
+++ src/VarArgs.java
@@ -2,9 +2,17 @@ import java.util.Arrays;
 
 public class VarArgs {
     public static void main(String[] args) {
-        fun(2,3,5,46,55,8,6);
+        fun(2, 3, 5, 46, 55, 8, 6);
+        multiple(1, 3, "Ankit", "name");
     }
-    public static void fun(int ...v){
+
+
+
+    static void multiple(int a, int b, String... v) {
+
+    }
+
+    public static void fun(int... v) {
         System.out.println(Arrays.toString(v));
     }
 }
