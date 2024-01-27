5bb43819d1aeddf34f79c6001f8092c362402c02
Ankit <ankit_1302@outlook.com>
2024-01-27 23:54:59 +0530

    ConditionalsLoops Assignment 1st and 2nd done

--- src/assignment/ConditionalsLoops.java
+++ src/assignment/ConditionalsLoops.java
@@ -5,7 +5,44 @@ public class ConditionalsLoops {
   /*
     1) Area Of Circle Java Program
     2) Area Of Triangle
-   */
+    3) Area Of Rectangle Program
+    4) Area Of Isosceles Triangle
+    5) Area Of Parallelogram
+    6) Area Of Rhombus
+    7) Area Of Equilateral Triangle
+    8) Perimeter Of Circle
+    9) Perimeter Of Equilateral Triangle
+    10) Perimeter Of Parallelogram
+    11) Perimeter Of Rectangle
+    12) Perimeter Of Square
+    13) Perimeter Of Rhombus
+    14) Volume Of Cone Java Program
+    15) Volume Of Prism
+    16) Volume Of Cylinder
+    17) Volume Of Sphere
+    18) Volume Of Pyramid
+    19) Curved Surface Area Of Cylinder
+    20) Total Surface Area Of Cube
+    21) Fibonacci Series In Java Programs
 
+
+   */
+  public static void main(String[] args) {
+//      System.out.println(question1(20));
+      System.out.println(question2(20));
+  }
+  static int question2(int sides_in_cm){
+      return sides_in_cm*3;
+  }
+  /*
+
+
+
+    static float question1(int r){
+        float pi = 3.14F;
+        return 2*pi*r;
+    }
+
+   */
 
 }
