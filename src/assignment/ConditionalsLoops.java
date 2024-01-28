34902d1910668e959ebcd40161daabd872c88bed
Ankit <ankit_1302@outlook.com>
2024-01-28 15:01:43 +0530

    ConditionalsLoops Assignment 20Question completed

--- src/assignment/ConditionalsLoops.java
+++ src/assignment/ConditionalsLoops.java
@@ -2,41 +2,123 @@ package assignment;
 
 public class ConditionalsLoops {
 
-  /*
-    1) Area Of Circle Java Program
-    2) Area Of Triangle
-    3) Area Of Rectangle Program
-    4) Area Of Isosceles Triangle
-    5) Area Of Parallelogram
-    6) Area Of Rhombus
-    7) Area Of Equilateral Triangle
-    8) Perimeter Of Circle
-    9) Perimeter Of Equilateral Triangle
-    10) Perimeter Of Parallelogram
-    11) Perimeter Of Rectangle
-    12) Perimeter Of Square
-    13) Perimeter Of Rhombus
-    14) Volume Of Cone Java Program
-    15) Volume Of Prism
-    16) Volume Of Cylinder
-    17) Volume Of Sphere
-    18) Volume Of Pyramid
-    19) Curved Surface Area Of Cylinder
-    20) Total Surface Area Of Cube
-    21) Fibonacci Series In Java Programs
+    /*
+      1) Area Of Circle Java Program
+      2) Area Of Triangle
+      3) Area Of Rectangle Program
+      4) Area Of Isosceles Triangle
+      5) Area Of Parallelogram
+      6) Area Of Rhombus // (d1*d2)/2
+      7) Area Of Equilateral Triangle // (Math.sqrt(3)/4)*Math.pow(side,2)
+      8) Perimeter Of Circle
+      9) Perimeter Of Equilateral Triangle
+      10) Perimeter Of Parallelogram
+      11) Perimeter Of Rectangle
+      12) Perimeter Of Square
+      13) Perimeter Of Rhombus
+      14) Volume Of Cone Java Program
+      15) Volume Of Prism
+      16) Volume Of Cylinder
+      17) Volume Of Sphere
+      18) Volume Of Pyramid
+      19) Curved Surface Area Of Cylinder
+      20) Total Surface Area Of Cube
+      21) Fibonacci Series In Java Programs
 
 
-   */
-  public static void main(String[] args) {
+     */
+    public static void main(String[] args) {
 //      System.out.println(question1(20));
-      System.out.println(question2(20));
+//      System.out.println(question2(20));
+//      System.out.println(question3(10,5));
+//        System.out.println(question4(5,4));
+//        System.out.println(question5(5,5));
+//        System.out.println(rhombus(10,10));
+//        System.out.println(EquilateralTriangle(5));
+//        System.out.println(parallelogramPari(20,10));
+//        System.out.println(rectPeri(20,10));
+//        System.out.println(periSq(5));
+//        System.out.println(periRhombus(5));
+//        System.out.println(volCone(5,6));
+//        System.out.println(volPrism(20,5));
+//        System.out.println(volCy(5,5));
+//        System.out.println(volSphere(5));
+//        System.out.println(volPyramid(5,3,6));
+//        System.out.println(csaCylinder(5,5));
+//        System.out.println(tsaCube(5));
+    }
+
+
+
+  /*
+
+
+static int tsaCube(int a){
+        return 6*a;
+}
+
+    static double csaCylinder(double r, double h) {
+        return 2 * Math.PI * r * h;
+    }
+
+    static float volPyramid(float l,float w,float h){
+        return (l*w*h)/3;
+    }
+
+static double volSphere(double r){
+    return 4.0/3.0*Math.PI*Math.pow(r,3);
+}
+
+static double volCy(double r,double h){
+        return Math.PI*Math.pow(r,2)*h;
+}
+
+
+static int volPrism(int l,int b){
+        return l*b;
+}
+
+static double volCone(double r ,double h){
+        return (1.0/3.0)*Math.PI*Math.pow(r,2)*h;
+}
+
+static int periRhombus(int a){
+        return 4*a;
+}
+
+    static int periSq(int a){
+        return a*4;
+    }
+
+    static int rectPeri(int l,int b){
+        return 2*(l+b);
+    }
+
+static int parallelogramPari(int l,int b){
+        return 2*(l+b);
+}
+
+ static double EquilateralTriangle(double side){
+        return (Math.sqrt(3)/4* Math.pow(side,2));
+ }
+static int rhombus(int d1,int d2){
+        return (d1*d2)/2;
+}
+static int question5(int base,int h){
+        return base*h;
+}
+
+    static int question4(int l,int i) {
+      return l*2*i;
+    }
+  static int question3(int l,int b){
+      return l*b;
   }
-  static int question2(int sides_in_cm){
+
+
+static int question2(int sides_in_cm){
       return sides_in_cm*3;
   }
-  /*
-
-
 
     static float question1(int r){
         float pi = 3.14F;
