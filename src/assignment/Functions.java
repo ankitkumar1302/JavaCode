bb6fe2b6122e11cd1e03a87262b7c6ada1db590c
ankitkumar1302 <ankit1302@outlook.com>
2024-03-23 00:06:34 +0530

    Function question

--- src/assignment/Functions.java
+++ src/assignment/Functions.java
@@ -1,22 +1,68 @@
 package assignment;
 
+
+/*
+Write a program to print the factorial of a number by defining a method named 'Factorial'.
+ Factorial of any number n is represented by n! and is equal to 1 * 2 * 3 * .... * (n-1) *n. E.g.-
+4! = 1 * 2 * 3 * 4 = 24
+3! = 3 * 2 * 1 = 6
+2! = 2 * 1 = 2
+Also,
+1! = 1
+0! = 1
+Write a function to find if a number is a palindrome or not. Take number as parameter.
+
+Convert the programs in flow of program, first java, conditionals & loops assignments into functions.
+
+Write a function to check if a given triplet is a Pythagorean triplet or not. (A Pythagorean triplet is when the sum of the square of two numbers is equal to the square of the third number).
+
+Write a function that returns all prime numbers between two given numbers.
+
+Write a function that returns the sum of first n natural numbers.
+ */
+
+import java.util.Scanner;
+
 public class Functions {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(Factorial(num));
 
+    }
+    public static int Factorial(int num) {
+        if (num == 0 || num == 1) {
+            return 1;
+        }
+        return num * Factorial(num - 1);
+    }
+}
+/*
+    Q4.
+       private static String isPrime(int num) {
+        if (num == 0) {
+            return "Enter the value other than 0";
+        }
+        if (num % 2 == 0)
+            return "Even";
 
-
+        else
+            return "Odd";
     }
 
 
 
-
-
-}
-/*
-
     Q3.A person is eligible to vote if his/her age is greater than or equal to 18.
      Define a method to find out if he/she is eligible to vote.
+
+  private static String age(int age) {
+        if (age >= 18) {
+            return "Your are eligible for vote";
+        }
+        else
+            return "you are not eligible";
+    }
 
 
 
